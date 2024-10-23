66adc65162f8af1b6f1eff475023eabb45273b34
Jon Olea <jonoolea@gmail.com>
2024-10-23 17:39:01 +0200

    scraper.js done

--- scripts/scraper.js
+++ scripts/scraper.js
@@ -8,22 +8,35 @@ let lortu = async () => {
         // Launch a new browser instance
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        
         // Navigate to the webpage
         await page.goto(url, { waitUntil: 'networkidle0' });
-        
+
         const data = await page.evaluate(() => {
-            const skills = Array.from(document.querySelectorAll('svg > text'));
+            const skills = Array.from(document.querySelectorAll('.svg-wrapper'));
             
-            return skills.map((skill, i) => ({
-                'id': i + 1,
-                'text': Array.from(skill.querySelectorAll('tspan')).reduce((acc, text) => {
-                    return acc + text.innerHTML + '\n\n\n';
-                }, ''),
-                'icon': argazkiak[i]
-            }));
+            return skills.map(skill => {
+                let id = skill.getAttribute('data-id'); //svg-wrapper-en data-id atributua lortu
+                let skill_texts = Array.from(skill.querySelectorAll('text > tspan')); // svg-wrapper-en textuak lortu
+                let full_text = skill_texts.map(tspan => tspan.textContent.trim()).join('\n\n\n'); // text barruko textuak array batean elkartu, gero '\n\n\n'-rekin elkartu string batean
+                let img = skill.querySelector('image').getAttribute('href').split('/').pop(); // argazkiaren izena lortu
+
+                return{
+                    'id': id,
+                    'text': full_text,
+                    'icon': img
+                }
+            });
         });
+        console.log(data);
+        fs.writeFileSync('../skills.json', JSON.stringify(data, null, 2));
+
+        console.log('Data saved to skills.json');
+
+        await browser.close();
+
         }catch(e){
             console.log('error:', e)
         }
 }
+
+lortu();
