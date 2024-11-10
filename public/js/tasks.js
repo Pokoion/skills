5d2c68a3cea808829fa75465971caff42ae3a706
Jon Olea <jonoolea@gmail.com>
2024-11-10 17:21:51 +0100

    enhance evidence submission feature; add validation and confetti effect on submit

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const evidence = document.querySelector('.skillEvidence');
     const checkboxes = document.querySelectorAll('input[type = "checkbox"]');
+    const submitButton = document.querySelector('#submitButton');
+    const evidenceText = document.querySelector('#evidence');
 
     // Checbox guztiak klikatzean, evidence erakutsi, denak ez badaude ezkutatu
     checkboxes.forEach(checkbox => {
@@ -14,4 +16,31 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-});
+    submitButton.addEventListener('click', () => {
+
+        if(evidenceText.value === '') {
+            alert('Enter a valid evidence please!'); 
+            return;
+        }
+
+        submitButton.disabled = true;
+        submitButton.style.cursor = 'not-allowed';
+
+        // Submit Evidence botoiaren posizioa lortu
+        const rect = submitButton.getBoundingClientRect();
+        
+        // Posizioa 0 eta 1 artean normalizatu
+        const x = (rect.left + rect.width / 2) / window.innerWidth;
+        const y = (rect.top + rect.height / 2) / window.innerHeight;
+
+        confetti({
+            particleCount: 300,
+            spread: 360,
+            startVelocity: 60,
+            scalar: 1,
+            origin: { x: x, y: y }
+         });
+
+    });
+
+}); 
